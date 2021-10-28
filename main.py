faa4b8604d38fe4a5f86f2b5b4e2fc237d0ccfc8
Facundo Manuel Otero <facu_otero@hotmail.com>
2021-10-28 08:21:35 -0300

    entrega1

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ nota_1 = int(input("Nota primera intancia evaluativa: "))
 
 nota_2 = int(input("Nota segunda intancia evaluativa: "))
 
-print(nombre + " tu nota final es: " + (nota_1*0.4+nota_2*0.6) )
+print(nombre + " tu nota final es: " + str(nota_1*0.4+nota_2*0.6) )
