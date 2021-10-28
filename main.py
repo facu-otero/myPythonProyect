37d355d769f3ed8fd8562c685caceb02e6abde35
Facundo Manuel Otero <facu_otero@hotmail.com>
2021-10-28 08:04:20 -0300

    puntapie inicial

---
+++ main.py
@@ -0,0 +1,8 @@
+nombre = input("coloca tu nombre: ")
+
+
+nota_1 = int(input("Nota primera intancia evaluativa: "))
+
+nota_2 = int(input("Nota segunda intancia evaluativa: "))
+
+print(nombre + " tu nota final es: " + (nota_1*0.4+nota_2*0.6) )
